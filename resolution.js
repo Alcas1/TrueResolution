455926bc844ff86cd8abff210454014280780f91
Peter Schaedler <pwschaedler@gmail.com>
2016-10-15 13:38:18 -0400

    Mark bug on Firefox at <= 50% zoom, add Opera test

--- resolution.js
+++ resolution.js
@@ -3,9 +3,10 @@
 // Browser Detection: http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
 
 // Currently tested on:
-// Firefox 49.0.1
+// Firefox 49.0.1 (bug at 50% and lower)
 // Safari 10.0
 // Chrome 53.0
+// Opera 40.0
 
 "use strict";
 
@@ -23,6 +24,9 @@ var onresize = function onresize() {
 
 	if (isFirefox) {
 		var pixelRatio = window.devicePixelRatio;
+
+		// BUG: Stops working on Firefox at zoom 50% and below.
+		// Turns to false for Retina display
 		var highdpi = (window.matchMedia && (window.matchMedia('only screen and (min-resolution: 124dpi), only screen and (min-resolution: 1.3dppx), only screen and (min-resolution: 48.8dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (min-device-pixel-ratio: 1.3)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 1.3);
 		
 		if (highdpi) {
